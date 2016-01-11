5fa29f753d0a1fadf97af6895fe85a6632472af8
yb <yb@nxc.no>
2016-01-11 21:29:48 +0200

    add fix for adding client list

--- src/main/webapp/resources/javascript/main.js
+++ src/main/webapp/resources/javascript/main.js
@@ -1,33 +1,43 @@
 "use strict";
-let inst = new uploadData({
-    "url": "ajax/admin/clients/addClientsList",
-    "componentRoot": document.querySelector("[data-component='userUploadCSVFile']"),
-    "btnFile": document.getElementById("uploadFile"),
-    "btnRenderTable": document.getElementById("renderTable"),
-    "btnSubmit": document.getElementById("submitData"),
-    "typeFields": [
-        {
-            "id": "firstName",
-            "title": "Имя"
-        },
-        {
-            "id": "lastName",
-            "title": "Фамилия"
-        },
-        {
-            "id": "email",
-            "title": "Почтовый адресс"
-        },
-        {
-            "id": "phoneNumber",
-            "title": "Номер Телефона"
-        }
-    ]
-});
+if(document.querySelector("[data-component='userUploadCSVFile']")) {
+    let inst = new uploadData({
+        "url": "ajax/admin/clients/addClientsList",
+        "componentRoot": document.querySelector("[data-component='userUploadCSVFile']"),
+        "btnFile": document.getElementById("uploadFile"),
+        "btnRenderTable": document.getElementById("renderTable"),
+        "btnSubmit": document.getElementById("submitData"),
+        "typeFields": [
+            {
+                "id": "firstName",
+                "title": "Имя"
+            },
+            {
+                "id": "lastName",
+                "title": "Фамилия"
+            },
+            {
+                "id": "email",
+                "title": "Почтовый адресс"
+            },
+            {
+                "id": "phoneNumber",
+                "title": "Номер Телефона"
+            }
+        ]
+    });
+}
 
-let dialog = new Dialog(
-    {
-        "component": "dialog",
-        "templateId": "dialog-edit-row"
-    }
-);
+if(document.querySelector("[data-component='dialog']")) {
+    let dialog = new Dialog(
+        {
+            "component": "dialog",
+            "templateId": "dialog-edit-row"
+        }
+    );
+}
+
+if(~~!window.location.search.substr(1).indexOf("saved")) {
+    noty({
+        text: 'Список был сохранён!'
+    });
+}
